9824a0a4caeb89be77cf7684d94cdc3fc1577235
Jithin Madduri <awesomejssm@gmail.com>
2022-02-25 13:27:40 -0500

    Initial Commit

---
+++ RUEats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RUEats
+//
+//  Created by Jithin Madduri on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
