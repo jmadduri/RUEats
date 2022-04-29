bcb4a62a7d837df0d22b3d8139d7e4ea539bf261
Aryan Patel <adp2582@gmail.com>
2022-04-29 14:00:14 -0400

    merged william's changes into the main branch

---
+++ RUEats/CreateUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateUserViewController.swift
+//  RUEats
+//
+//  Created by William Wu on 4/29/22.
+//
+import UIKit
+
+class CreateUserViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var netIDTextField: UITextField!
+    @IBOutlet weak var rutgersEmailTextField: UITextField!
+    @IBOutlet weak var createPasswordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        netIDTextField.delegate = self
+        rutgersEmailTextField.delegate = self
+        createPasswordTextField.delegate = self
+        confirmPasswordTextField.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func createNewUserButtonTapped(_ sender: Any) {
+    }
+    
+}
